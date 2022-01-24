4d5fc4545a57b37850e8c50dabc87060bdd52eed
SK Choi <seokkeunchoi@gmail.com>
2022-01-24 11:58:03 -0700

    First Commit

---
+++ Oscilloscope/Step.cs
@@ -0,0 +1,34 @@
+﻿using OpenTap;   // Use OpenTAP infrastructure/core components (log,TestStep definition, etc)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oscilloscope
+{
+    [Display("IDN", Group: "Oscilloscope", Description: "Check the instrument connection")]
+    public class Step : TestStep
+    {
+        #region Settings
+        // ToDo: Add property here for each parameter the end user should be able to change.
+        [Display("Instrument")]
+        public scope OSC { get; set; }
+        #endregion
+
+        public Step()
+        {
+            // ToDo: Set default values for properties / settings.
+        }
+
+        public override void Run()
+        {
+            string idn = OSC.ScpiQuery("*IDN?");
+            // ToDo: Add test case code.
+            RunChildSteps(); //If the step supports child steps.
+            Log.Info(idn);
+            // If no verdict is used, the verdict will default to NotSet.
+            // You can change the verdict using UpgradeVerdict() as shown below.
+            // UpgradeVerdict(Verdict.Pass);
+        }
+    }
+}
---
+++ Oscilloscope/scope.cs
@@ -0,0 +1,55 @@
+﻿using OpenTap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+//Note this template assumes that you have a SCPI based instrument, and accordingly
+//extends the ScpiInstrument base class.
+
+//If you do NOT have a SCPI based instrument, you should modify this instance to extend
+//the (less powerful) Instrument base class.
+
+namespace Oscilloscope
+{
+    [Display("scope", Group: "Oscilloscope", Description: "Oscilloscope connection")]
+    public class scope : ScpiInstrument
+    {
+        #region Settings
+        // ToDo: Add property here for each parameter the end user should be able to change
+        #endregion
+
+        public scope()
+        {
+            Name = "Scope";
+            // ToDo: Set default values for properties / settings.
+        }
+
+        /// <summary>
+        /// Open procedure for the instrument.
+        /// </summary>
+        public override void Open()
+        {
+
+            base.Open();
+            // TODO:  Open the connection to the instrument here
+
+            //if (!IdnString.Contains("Instrument ID"))
+            //{
+            //    Log.Error("This instrument driver does not support the connected instrument.");
+            //    throw new ArgumentException("Wrong instrument type.");
+            // }
+
+        }
+
+        /// <summary>
+        /// Close procedure for the instrument.
+        /// </summary>
+        public override void Close()
+        {
+            // TODO:  Shut down the connection to the instrument here.
+            base.Close();
+        }
+    }
+}
